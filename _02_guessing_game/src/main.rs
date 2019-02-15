e4b64a6bceb38d78d877ba2f8aa4cb1774276e7c
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-02-14 20:41:40 -0700

    chapter 2 complete

--- _02_guessing_game/src/main.rs
+++ _02_guessing_game/src/main.rs
@@ -1,29 +1,32 @@
-use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
+use std::io;
 
 fn main() {
-    
-    let mut guess = String::new();
-   
-    io::stdin().read_line(&mut guess)
-        .expect("REEEEE Failed to read line");
-   
-    let guess: u32 = guess.trim().parse()
-        .expect("REEEEE Type a number!");
-
-    let secret_number = rand::thread_rng().gen_range(1,101);
-
     println!("Guess the number!");
     println!("What's your guess? : ");
 
+    let secret_number = rand::thread_rng().gen_range(1, 101);
 
+    loop {
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("REEEEE Failed to read line");
+        let guess: u32 = match guess.trim().parse(){
+        Ok(num) => num,
+           Err(_) => continue,
+        };
 
-    println!("You guessed {}", guess);
+        println!("You guessed {}", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
